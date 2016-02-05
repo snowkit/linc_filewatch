ec82ac5f70ee3156d94b4421f5780ac5bf427038
underscorediscovery <sven@underscorediscovery.com>
2016-02-04 21:15:28 -0330

    update test for better layout

--- test/Test.hx
+++ test/Test.hx
@@ -7,28 +7,66 @@ import filewatch.Filewatch;
 
 class Test {
 
+    static var start_time = 0.0;
+    static var looping = true;
+    static var step = 0;
+    static var file = 'test.file';
+
     static function main() {
 
-        trace('start');
+        start_time = Date.now().getTime();
+
+        trace('start ' + start_time);
+
+        var cwd = Sys.getCwd();
+        file = cwd + file;
+
+        trace('cwd is : $cwd');
+        trace('file should be : $file');
 
         Filewatch.init(function(event:FilewatchEvent) {
             trace('type: ${event.type} path: ${event.path}');
         });
 
-        Filewatch.add_watch(Sys.getCwd());        
+        Filewatch.add_watch(cwd);
 
-        var idx = 0;
-        while(idx < 4) {
-            idx++;
-            sys.io.File.saveContent('./test.file$idx', 'linc filewatch ' + Math.random());
-            Sys.sleep(1);
-            sys.FileSystem.deleteFile('./test.file$idx');
-            Sys.sleep(1);
-        }
-
+        while(looping) loop();
+        
         Filewatch.shutdown();
 
-        trace('exit');
+        trace('done');
+
+    } //main
+
+
+    static function loop() {
+
+        Filewatch.update();
+
+        var time_since_start : Float = (Date.now().getTime() - start_time)/1000;
+
+        if(time_since_start > 4) {
+            looping = false;
+            return;
+        }
+
+        if(time_since_start == 1 && step != 1) {
+            step++;
+            trace('step 1 - create');
+            sys.io.File.saveContent(file, 'linc filewatch ' + Math.random());
+        }
+
+        if(time_since_start == 2 && step != 2) {
+            step++;
+            trace('step 2 - modify');
+            sys.io.File.saveContent(file, 'linc filewatch ' + Math.random());
+        }
+
+        if(time_since_start == 3 && step != 3) {
+            step++;
+            trace('step 3 - remove');
+            sys.FileSystem.deleteFile(file);
+        }
 
     }
 
